76e96524a54b02f5b2b0944cab3c3c352bc29511
ahmed <ahmed hadjeres>
2019-12-17 09:42:46 +0100

    updated the template

--- src/template.js
+++ src/template.js
@@ -1,24 +1,28 @@
 const fs = require('fs');
 const fsPromises = fs.promises;
 
-const Dataset = "../dataset/day0-1"
+const Dataset = "../dataset/day3"
 
 class App {
     constructor({dataset}) {
-      this.dataset = dataset;
-
-    }
+        this.dataset = dataset;
+  
+      }
+  
+      async loadDataFromFile(){
+          try{
+              const data = await fsPromises.readFile(this.dataset, 'utf8');
+              return data;
+          }catch(e){
+              console.log(e);
+              throw e;
+          }
+      }
     async run(){
         console.log(`APP started with ${this.dataset}`)
-        
-        try{
-            const data = await fsPromises.readFile(this.dataset, 'utf8');
-            console.log(data);
-        }catch(e){
-            console.log(e);
-            throw e;
-        }
+        let rawData = await this.loadDataFromFile();
     }
   }
+  
   let app = new App({dataset: Dataset});
   app.run();
